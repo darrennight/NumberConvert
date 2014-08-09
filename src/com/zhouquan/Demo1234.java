9574ecf8fd4d26034c30dad91216523c7bcbe818
gaoshanliushui <1083879035@qq.com>
2014-08-10 00:40:08 +0800

    2

--- src/com/zhouquan/Demo1234.java
+++ src/com/zhouquan/Demo1234.java
@@ -177,22 +177,103 @@ public class Demo1234 {
 		return result.toString();
 	}
 	
+	public static String convert4(double money){
+		//：“壹、贰、叁、肆、伍、陆、柒、捌、玖、拾、佰、仟、萬
+		char[] ch1 = new char[]{'零','壹','贰','叁','肆','伍','陆','柒','捌'};
+		char[] ch2 = new char[]{'分','角','元','拾','佰','仟','萬','拾','佰','仟',
+								'亿','拾','佰','仟','萬','拾','佰','仟','亿'};
+		int index = 0;
+		int weight = 0;
+		long count = (long) (money*100);
+		StringBuffer sb = new StringBuffer();
+		while(count!=0){
+			index = (int) (count % 10);
+//			System.out.println(index);
+			sb.append(ch2[weight]);  //添加单位
+			sb.append(ch1[index]);  //完成数字到字符的转换
+			count = count / 10;
+			weight++;
+		}
+		
+		//对字符串进行处理，使之符合人的习惯
+		StringBuffer result = new StringBuffer();
+		result = sb.reverse();
+		//2000 0000 0401.0012
+//		System.out.println(result.toString());
+		int i = 0;
+		while(i < result.length()){
+			int j = i + result.substring(i).indexOf('零');
+			if(j<i)
+				break;  //说明不存在零这个特殊情况
+			else{
+				char nextch = result.charAt(j+1);
+				switch(nextch)
+				{
+				case '拾':
+				case '佰':
+				case '仟':
+					if(result.charAt(j+2)!='零'){
+						result.deleteCharAt(j+1);//删除单位
+						i=j+1;
+					}else{
+						result.delete(j, j+2); //删除零和单位
+						i=j;
+					}
+					break;
+				case '萬':
+					if(result.charAt(j-1)!='亿'){
+						result.deleteCharAt(j);  //删除零
+						i=j+1;
+					}else{
+						result.delete(j, j+2);//删除零和单位
+						i=j;
+					}
+					break;
+				case '亿':
+						result.deleteCharAt(j);  //删除零
+						i=j+1;
+					break;
+				case '元':
+					result.deleteCharAt(j); 
+					break;
+				case '角':
+					result.deleteCharAt(j+1);//删除单位
+					i=j+1;
+					break;
+				case '分':
+					if(result.charAt(j-1)!='角'){
+						result.delete(j-1, j+2);
+						result.append('整');
+					}
+					i=result.length();
+					break;
+					
+				}
+			} 
+		}
+		return result.toString();
+	}
+	
 	public static void main(String[] args) {
 		String str1 = new String();
 		String str2 = new String();
 		String str3 = new String();
+		//922 3372 0368 5477 5808
 		int[] test = new int[]{200000401,204060801,201254012,201204012,234233343,1200000000};
-		double[] test2 = new double[]{2000401.0012,2040801.1023,2012012.01254,2014012.1244};
+		double[] test2 = new double[]{200000000401.0012,2041232330801.1023,
+				2022300003012012.01254,40000000000000000.1244};
 		for(int i = 0;i<test2.length;i++){
 //			str1 = Demo1234.convert(test[i]);
 //			str2 = Demo1234.convert2(test[i]);
 ////			System.out.println("第" + i + "个数" + test[i] + str1);
 //			System.out.println("第" + i + "个数" + test[i] +str2);
 			
-			str3 = Demo1234.convert3(test2[i]);
-			System.out.println("第" + i + "个数" + test2[i] +str3);
+//			str3 = Demo1234.convert4(40000000000000000.1244);
+//			System.out.println("第" + i + "个数" + test2[i] +str3);
 		}
-			
+		//注意java long的最大数据为922 3372 0368 5477 5808，但此处却超出了double类型表示的范围
+		str3 = Demo1234.convert4(40000000000000000.1244);
+		System.out.println(str3);
 	}
 	
 }
